e64b27bd5c2890915631c9794adeafdf408b386e
Samuel Tucker <scubasam@live.co.uk>
2020-01-07 12:22:24 +0000

    added documentation

--- mypackage/R/sum_squares.R
+++ mypackage/R/sum_squares.R
@@ -1,3 +1,11 @@
+#' calculate the sums of squares for a sample
+#'
+#' @param v numeric vector
+#'
+#' @return number
+#' @export
+#'
+#' @examples
 sum_squares <- function(v){
   sum(v - mean(v))^2
 }
